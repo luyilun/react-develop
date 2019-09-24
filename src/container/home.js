b8f4536e202cf3de9600426ce388b8762532c14d
yilun lu <936894476@qq.com>
2019-09-24 15:46:21 +0800

    新增异步函数编译

--- src/container/home.js
+++ src/container/home.js
@@ -11,6 +11,27 @@ export default class Home extends React.Component{
 			})
 		})*/
 		console.log(this.props);
+		this.doIt();
+	}
+	doIt= async ()=>{
+		const a = await this.get1(0);
+		const b = await this.get2(a);
+		const c = await this.get3(b);
+		console.log(a, b, c);
+	}
+	course = (a) => {
+		return new Promise((resolve)=>{
+			setTimeout(()=>resolve(a + 1), 200);
+		});
+	}
+	get1 = async (a) => {
+		return this.course(a);
+	}
+	get2 = async (a) => {
+		return this.course(a);
+	}
+	get3 = async (a) => {
+		return this.course(a);
 	}
 	render(){
 		return (
